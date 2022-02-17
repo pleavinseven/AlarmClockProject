da97ef8b5383dbc3dd75cdb58576398b09fc4d35
pleav <pleavinseven@gmail.com>
2022-02-17 17:52:42 +0100

    Main activity no longer extends TimePickerDialog.
    Time Picker is now created through an instance of Calendar, this removes the forced onTimeSet function.

--- app/src/main/java/com/pleavinseven/alarmclockproject/MainActivity.kt
+++ app/src/main/java/com/pleavinseven/alarmclockproject/MainActivity.kt
@@ -3,30 +3,27 @@ package com.pleavinseven.alarmclockproject
 import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.pleavinseven.alarmclockproject.databinding.ActivityMainBinding
 import com.pleavinseven.alarmclockproject.settings.SettingsActivity
+import java.util.*
 
-class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
+class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var time: String
-    var hour = 0
-    var minute = 0
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.title = ""
-        var button:String = "" // pass this to onTimeSet
 
 //        TODO: set a scroll view here and set btn_new_alarm to create new xml data
 
@@ -44,25 +41,21 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
 
 
     private fun setTime(button: Button) {
-        //pick a better theme/create a custom theme
-        TimePickerDialog(
-            this,
-            android.R.style.Theme_Material_Dialog_NoActionBar, //TODO: set in drawable, so its changeable
-            this,
-            hour,
-            minute,
-            true
-        ).show()
+        Calendar.getInstance().apply {
+            //pick a better theme/create a custom theme
+            TimePickerDialog(
+                this@MainActivity,
+                android.R.style.Theme_Material_Dialog_NoActionBar, //TODO: set in drawable, so its changeable
+                {_, hour, minute ->
+                    this.set(Calendar.HOUR_OF_DAY, hour)
+                    this.set(Calendar.MINUTE, minute)
+                },
+                this.get(Calendar.HOUR_OF_DAY),
+                this.get(Calendar.MINUTE),
+                true
+            ).show()
+        }
     }
-
-    override fun onTimeSet(view: TimePicker?, hour: Int, minute: Int) {
-        if (minute > 9)
-            time = "$hour:$minute"
-        else
-            time = "$hour:0$minute"
-        binding.tvNextAlarm.text = time
-    }
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -79,12 +72,3 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
         }
     }
 }
-
-
-
-
-// text to speech? choose your own morning message --> api
-// barcode scanner?
-// spotify api? --> probably not
-// weather? --> api
-//
