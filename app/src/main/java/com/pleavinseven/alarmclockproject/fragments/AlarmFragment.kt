683f968fcdf0a9824874e13d9b891c8f63d20694
joe <pleavinseven@gmail.com>
2022-02-21 23:06:29 +0100

    - Basic AlarmFragment created for testing functionality

---
+++ app/src/main/java/com/pleavinseven/alarmclockproject/fragments/AlarmFragment.kt
@@ -0,0 +1,25 @@
+package com.pleavinseven.alarmclockproject.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pleavinseven.alarmclockproject.R
+import com.pleavinseven.alarmclockproject.databinding.FragmentAlarmBinding
+
+
+
+class AlarmFragment : Fragment() {
+
+    lateinit var binding: FragmentAlarmBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlarmBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
