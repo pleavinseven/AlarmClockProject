46d43d0bdc37cb583522a60efc851c3a0a0ff4be
pleavinseven <pleavinseven@gmail.com>
2022-02-25 16:23:44 +0100

    - added snooze option

--- app/src/main/java/com/pleavinseven/alarmclockproject/AlarmRingActivity.kt
+++ app/src/main/java/com/pleavinseven/alarmclockproject/AlarmRingActivity.kt
@@ -4,14 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.*
-import android.os.VibrationAttributes.USAGE_ALARM
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.pleavinseven.alarmclockproject.databinding.ActivityAlarmRingBinding
+import java.util.*
 
 class AlarmRingActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityAlarmRingBinding
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,29 +23,51 @@ class AlarmRingActivity : AppCompatActivity() {
         supportActionBar?.title = ""
 
 
-
         val ring = MediaPlayer.create(this, R.raw.finch)
         ring.isLooping = true
         ring.start()
         val vibe =
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val vibratorManager =
-                getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                val vibratorManager =
+                    getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
                 vibratorManager.defaultVibrator
             } else {
                 getSystemService(VIBRATOR_SERVICE) as Vibrator
-        }
+            }
 
-        vibe.vibrate(VibrationEffect.createWaveform(longArrayOf(200,1000,500,500),0))
+        vibe.vibrate(VibrationEffect.createWaveform(longArrayOf(200, 1000, 500, 500), 0))
+        // TODO: set vibrate pattern options?
 
 
 
 
-        binding.btnCancelAlarm.setOnClickListener{
+        binding.btnCancelAlarm.setOnClickListener {
             ring.stop()
             vibe.cancel()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnSnoozeAlarm.setOnClickListener{
+            val alarmId = Random().nextInt(Integer.MAX_VALUE)
+            val calendar: Calendar = Calendar.getInstance()
+            val snoozeMins = 10
+            calendar.timeInMillis = System.currentTimeMillis()
+            calendar.add(Calendar.MINUTE, snoozeMins) // TODO: set snooze length options
+            val alarm = AlarmManager(
+                alarmId,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                "Snooze",
+                started = true,
+                recurring = false
+            )
+            Toast.makeText(this, "Alarm snoozed for $snoozeMins minutes", Toast.LENGTH_SHORT).show()
+            ring.stop()
+            vibe.cancel()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            alarm.schedule(this)
         }
     }
 
