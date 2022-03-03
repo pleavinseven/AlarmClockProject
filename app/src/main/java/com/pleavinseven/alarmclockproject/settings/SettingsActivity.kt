a0884223a2823ba7feb39e21403c3c08c4008700
pleavinseven <pleavinseven@gmail.com>
2022-03-03 23:44:24 +0100

    - Day/Night mode switch enabled.

--- app/src/main/java/com/pleavinseven/alarmclockproject/settings/SettingsActivity.kt
+++ app/src/main/java/com/pleavinseven/alarmclockproject/settings/SettingsActivity.kt
@@ -1,16 +1,22 @@
 package com.pleavinseven.alarmclockproject.settings
 
+import android.app.UiModeManager
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.pleavinseven.alarmclockproject.R
 import com.pleavinseven.alarmclockproject.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
+    private lateinit var uiModeManager: UiModeManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +38,37 @@ class SettingsActivity : AppCompatActivity() {
             else -> false
         }
     }
+
+
+
 }
 
 class SettingsFragment: PreferenceFragmentCompat(){
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        sharedPreferences = context?.getSharedPreferences("nightModePrefs", Context.MODE_PRIVATE)!!
+
+        val spDarkMode = findPreference<SwitchPreference>("sp_dark_mode")
+        spDarkMode!!.setOnPreferenceClickListener {
+            if(spDarkMode.isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                saveNightModeState(true)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                saveNightModeState(true)
+            }
+            true
+        }
+
+    }
+
+    private fun saveNightModeState(night: Boolean) {
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putBoolean("isNight", night)
+        editor.apply()
     }
 }
