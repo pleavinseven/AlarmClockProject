b3556655264244c765ef1741d87cc644111b7901
pleavinseven <pleavinseven@gmail.com>
2022-03-20 00:49:47 +0100

    - updated database to include a started column, when alarm.started is true then alarm can be started from boot

--- app/src/main/java/com/pleavinseven/alarmclockproject/service/AlarmRingService.kt
+++ app/src/main/java/com/pleavinseven/alarmclockproject/service/AlarmRingService.kt
@@ -2,7 +2,9 @@ package com.pleavinseven.alarmclockproject.service
 
 import android.app.*
 import android.content.Intent
+import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.os.*
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationManagerCompat
@@ -14,14 +16,21 @@ import com.pleavinseven.alarmclockproject.R
 class AlarmRingService : Service() {
 
     private var mPlayer: MediaPlayer? = null
-
+    val alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
         createNotificationChannel()
-        mPlayer = MediaPlayer.create(this, R.raw.finch)
+        mPlayer = MediaPlayer()
+        mPlayer?.setAudioAttributes(
+            AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+        )
+        mPlayer?.setDataSource(this, alarmUri)
+        mPlayer?.prepare()
         mPlayer?.isLooping = true
         mPlayer?.start()
     }
@@ -30,7 +39,7 @@ class AlarmRingService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         val notificationIntent = Intent(this, AlarmRingActivity::class.java)
-            notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
 
         val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
 
@@ -44,7 +53,6 @@ class AlarmRingService : Service() {
             .build()
 
         NotificationManagerCompat.from(this).notify(1, builder.build())
-
 
 
         val ringIntent = Intent(this, AlarmRingActivity::class.java)
