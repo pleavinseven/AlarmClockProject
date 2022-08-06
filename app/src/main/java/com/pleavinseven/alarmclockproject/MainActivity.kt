8fbb42b431332125e02cd4b8c2ad397f7e7d2333
pleavinseven <pleavinseven@gmail.com>
2022-08-06 03:20:08 +0200

    removed background

--- app/src/main/java/com/pleavinseven/alarmclockproject/MainActivity.kt
+++ app/src/main/java/com/pleavinseven/alarmclockproject/MainActivity.kt
@@ -11,8 +11,6 @@ import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
-import androidx.preference.SwitchPreference
 import com.pleavinseven.alarmclockproject.databinding.ActivityMainBinding
 import com.pleavinseven.alarmclockproject.settings.SettingsActivity
 
@@ -36,15 +34,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun actionBarColour() {
         val actionBar = supportActionBar
-        actionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#018786")))
+        actionBar?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         actionBar?.title = ""
         actionBar?.elevation = 0F
     }
 
     private fun backGroundColour() {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.statusBarColor = Color.parseColor("#018786")
-        window.setBackgroundDrawableResource(R.drawable.gradient_background)
+        window.statusBarColor = Color.parseColor("#F0EDFF")
+        window.setBackgroundDrawable(ColorDrawable(Color.parseColor("#CDCCF0")))
     }
 
     private fun navBarColour() {
@@ -66,25 +64,25 @@ class MainActivity : AppCompatActivity() {
                 window.navigationBarColor = Color.parseColor("#795f56")
             }
             Configuration.UI_MODE_NIGHT_NO -> {
-                window.navigationBarColor = Color.parseColor("#F3b9a4")
+                window.navigationBarColor = Color.parseColor("#F0EDFF")
             }
             Configuration.UI_MODE_NIGHT_UNDEFINED -> {
-                window.navigationBarColor = Color.parseColor("#F3b9a4")
+                window.navigationBarColor = Color.parseColor("#F0EDFF")
             }
         }
     }
 
-    private fun setDayNight(){
+    private fun setDayNight() {
         sharedPreferences = getSharedPreferences("nightModePrefs", Context.MODE_PRIVATE)!!
         val nightMode = sharedPreferences.getBoolean("isNight", true)
-        if (nightMode){
+        if (nightMode) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }  else {
+        } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
