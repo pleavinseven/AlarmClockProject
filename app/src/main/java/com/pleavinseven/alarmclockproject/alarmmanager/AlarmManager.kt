2a6b2b73b817f21d7263a22543ea10222d41e7a3
pleavinseven <pleavinseven@gmail.com>
2022-09-08 15:32:45 +0200

    unused imports removed

--- app/src/main/java/com/pleavinseven/alarmclockproject/alarmmanager/AlarmManager.kt
+++ app/src/main/java/com/pleavinseven/alarmclockproject/alarmmanager/AlarmManager.kt
@@ -4,9 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.widget.Toast
-import com.pleavinseven.alarmclockproject.data.model.Alarm
 import com.pleavinseven.alarmclockproject.receiver.AlarmReceiver
 import java.util.*
 
